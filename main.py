3b7948290276a172d85543045e0bc920e8e5cf7f
Samoylik <vasya.samoylik.rom@gmail.com>
2026-03-19 21:56:45 +0300

    logic changed

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import datetime
 
 log_file = "log.txt"
-message = f"{datetime.datetime.now()} <- это время запуска скрипта\n"
-
-with open(log_file, "a", encoding="utf-8") as f:
-    f.write(message)
+counter = 0
+text = ''
+with open(log_file, 'r', encoding="utf-8") as f:
+    counter = int(f.readline())
+counter += 1
+with open(log_file, 'w', encoding="utf-8") as f:
+    f.write(str(counter))
